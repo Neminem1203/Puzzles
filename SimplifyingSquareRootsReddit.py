6927650d834a9ca1843803b94a7c7fe2bbde7826
neminem1203 <tpaul1203@gmail.com>
2017-04-26 23:32:15 -0400

    What I've done so far

---
+++ SimplifyingSquareRootsReddit.py
@@ -0,0 +1,40 @@
+import math
+
+def findSquares(num):
+    for i in range(2, (num + 2) // 2 ):
+        if(num % (i*i)  == 0):
+            return i
+    return -1
+
+def GCD(num1, num2):
+    if(num1 > num2):
+        num = num2
+    else:
+        num = num1
+
+    for i in range(num,1,-1):
+        if(num1 % i == 0) and (num2 % i == 0):
+            return i
+    return 1
+
+for i in range(2,2,-1):
+    print(i)
+
+numList = input("Enter the 4 numbers: ").split()
+if(len(numList) != 4):
+    print("Error. Not 4 numbers.", len(numList), "numbers input")
+else:
+    print(numList[0],"√",numList[1],"/",numList[2],"√",numList[3])
+    numList[1] = int(numList[1]) * int(numList[3])
+    numList[2] = int(numList[2]) * int(numList[3])
+    del numList[3]
+    while(findSquares(int(numList[1])) != -1):
+        num = findSquares(int(numList[1]))
+        numList[0] = num * int(numList[0])
+        numList[1] = int(numList[1]) / (num*num)
+
+    GCDNum = GCD(int(numList[0]), int(numList[2]))
+    if(GCDNum != 1):
+        numList[0] = int(numList[0]) / GCDNum
+        numList[2] = int(numList[2]) / GCDNum
+    print(numList)
---
+++ nextLargestNumberReddit.py
@@ -0,0 +1,33 @@
+
+beginNum = input("Input Number: ")
+
+indexOfSwap = -1
+nonZeroMinIndex = 0
+increasing = beginNum[len(beginNum)-1]
+for i, value in enumerate(reversed(beginNum)):
+    if(increasing > value):
+        indexOfSwap = len(beginNum) - (i + 1)
+        break
+    else:
+        if(nonZeroMinIndex == 0 and (value != '0')):
+            nonZeroMinIndex = len(beginNum) - (i + 1)
+        increasing = value
+
+
+if indexOfSwap != -1:
+    newNum = ""
+    for i, value in enumerate(beginNum):
+        if(i < indexOfSwap):
+            newNum += value
+        else:
+            newNum += beginNum[nonZeroMinIndex]
+            for ind in range(len(beginNum)-1, i, -1):
+                if(beginNum[ind] > value):
+                    newNum += value
+                    value = ""
+                if(ind != nonZeroMinIndex):
+                    newNum += beginNum[ind]
+            break
+    print(newNum)
+else:
+    print("Can't go higher")
