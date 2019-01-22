79487dc16f8d41a0dd05681cf2901113809d3c81
Paul <Neminem1203@users.noreply.github.com>
2019-01-22 00:29:33 -0500

    halfway through inplaceRotation
    started out the project inplace rotation and will probably finish when i have free time

---
+++ inplaceRotation.py
@@ -0,0 +1,56 @@
+'''
+https://www.geeksforgeeks.org/inplace-rotate-square-matrix-by-90-degrees/
+Given an square matrix, turn it by 90 degrees in anti-clockwise direction without using any extra space.
+'''
+import math
+testMatrix1 = [[1,2,3],[4,5,6],[7,8,9]]
+testMatrix2 = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+testMatrix3 = [[1,2,3,4,5,6],[7,8,9,10,11,12],[13,14,15,16,17,18],
+               [19,20,21,22,23,24],[25,26,27,28,29,30],[31,32,33,34,35,36]]
+
+
+
+
+def shift(magicNumbers):
+    shiftNumbers = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+    for i in range(4):
+        for j in range(2):
+            magicNumbers[i][j] += shiftNumbers[i][j]
+    return magicNumbers
+
+def swapInd(matrix, arrpos):
+    ind = 1
+    while(ind != len(arrpos)-2):
+        temp = matrix[arrpos[0][0]][arrpos[0][1]]
+        matrix[arrpos[0][0]][arrpos[0][1]] = matrix[arrpos[ind][0]][arrpos[ind][1]]
+        matrix[arrpos[ind][0]][arrpos[ind][1]] = temp
+        ind += 1
+    return matrix
+
+
+def inplaceRotation(mat):
+    if(not mat):
+        return mat
+    n = len(mat)-1
+    mN = [[0, 0], [n, 0], [n, n], [0, n]] #magic numbers
+    for i in range(math.ceil((n+1)/2)):
+        # for i in magicNumbers:
+        #     print(i,end=", ")
+        # print("")
+        for j in range(math.ceil((n+1)/2)):
+            # displacement of [0][j], [-j][0], [0][-j], [j][0]
+            # print(mat   [mN[0][0]]      [mN[0][1]+j]) # pos0 -> pos1
+            # print(mat   [mN[1][0]-j]    [mN[1][1]]) # pos1 -> pos2
+            # print(mat   [mN[2][0]]      [mN[2][1]-j]) # pos2 -> pos3
+            # print(mat   [mN[3][0]+j]    [mN[3][1]]) # pos3 -> pos0
+            swapPositions = [[mN[0][0],   mN[0][1]+j],
+                             [mN[1][0]-j, mN[1][1]],
+                             [mN[2][0],   mN[2][1]-j],
+                             [mN[3][0]+j, mN[3][1]]]
+            mat = swapInd(mat, swapPositions)
+        mN = shift(mN)
+    return mat
+
+
+print(inplaceRotation(testMatrix1))
+print(inplaceRotation(testMatrix2))
--- sumOfBitDifference.py
+++ sumOfBitDifference.py
@@ -2,26 +2,6 @@
 https://www.geeksforgeeks.org/sum-of-bit-differences-among-all-pairs/
 Given an integer array of n integers, find sum of bit differences in all pairs that can be formed from array elements. Bit difference of a pair (x, y) is count of different bits at same positions in binary representations of x and y.
 For example, bit difference for 2 and 7 is 2. Binary representation of 2 is 010 and 7 is 111 ( first and last bits differ in two numbers).
-
-Examples :
-
-Input: arr[] = {1, 2}
-Output: 4
-All pairs in array are (1, 1), (1, 2)
-                       (2, 1), (2, 2)
-Sum of bit differences = 0 + 2 +
-                         2 + 0
-                      = 4
-
-Input:  arr[] = {1, 3, 5}
-Output: 8
-All pairs in array are (1, 1), (1, 3), (1, 5)
-                       (3, 1), (3, 3) (3, 5),
-                       (5, 1), (5, 3), (5, 5)
-Sum of bit differences =  0 + 1 + 1 +
-                          1 + 0 + 2 +
-                          1 + 2 + 0
-                       = 8
 '''
 import math
 
