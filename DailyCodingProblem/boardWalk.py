b455ce02a5d6dec05581c1b0312802564eea0188
Paul <Neminem1203@users.noreply.github.com>
2019-03-04 21:31:46 -0500

    boardWalk done
    finished day 23

--- DailyCodingProblem/boardWalk.py
+++ DailyCodingProblem/boardWalk.py
@@ -16,16 +16,50 @@ and start = (3, 0) (bottom left) and end = (0, 0) (top left), the minimum number
 
 
 # normal mode
-normalMaze = [  ['f', 'f', 'f', 'f'],
-                ['t', 't', 'f', 't'],
-                ['f', 'f', 'f', 'f'],
-                ['f', 'f', 'f', 'f']]
+normal = [  ['f', 'f', 'f', 'f'],
+            ['t', 't', 'f', 't'],
+            ['f', 'f', 'f', 'f'],
+            ['f', 'f', 'f', 'f']]
+# answer = 7
+normalStart = [3,0]
+normalEnd = [0,0]
 
 # hard mode
-longMaze = [    ['f','f','f','f','f','f','f','f','f'],
-                ['t','t','t','t','t','t','t','t','f'],
-                ['f','f','f','t','f','f','f','t','f'],
-                ['f','t','f','t','f','t','f','t','f'],
-                ['f','t','f','f','f','t','f','f','f']]
+long = [    ['f','f','f','f','f','f','f','f','f'],
+            ['t','t','t','t','t','t','t','t','f'],
+            ['f','f','f','t','f','f','f','t','f'],
+            ['f','t','f','t','f','t','f','t','f'],
+            ['f','t','f','f','f','t','f','f','f']]
+# answer = 28
+longStart = [4,0]
+longEnd = [0,0]
 
 
+def dfs(maze,start, end):
+    width = len(maze[0])
+    height = len(maze)
+    start.append(0)
+    listOfPos = [start]
+    while(listOfPos):
+        # print(listOfPos)
+        thisPos = listOfPos.pop(0)
+        maze[thisPos[0]][thisPos[1]] = 't'
+        if(abs(thisPos[0] - end[0]) + abs(thisPos[1] - end[1]) == 1):
+            return thisPos[2]+1
+        if thisPos[0]-1 >= 0 and maze[thisPos[0] - 1][ thisPos[1]] == 'f':
+            listOfPos.append([thisPos[0] - 1, thisPos[1], thisPos[2] + 1])
+            # maze[thisPos[0] - 1][thisPos[1]] = 't'
+        if thisPos[0]+1 < height and maze[thisPos[0] + 1][ thisPos[1]] == 'f':
+            listOfPos.append([thisPos[0] + 1, thisPos[1], thisPos[2] + 1])
+            # maze[thisPos[0] + 1][thisPos[1]] = 't'
+
+        if thisPos[1]-1 >= 0 and maze[thisPos[0]][ thisPos[1] - 1] == 'f':
+            listOfPos.append([thisPos[0], thisPos[1] - 1, thisPos[2] + 1])
+            # maze[thisPos[0]][thisPos[1] - 1] = 't'
+        if thisPos[1]+1 < width and maze[thisPos[0]][ thisPos[1] + 1] == 'f':
+            listOfPos.append([thisPos[0], thisPos[1] + 1, thisPos[2] + 1])
+            # maze[thisPos[0]][thisPos[1] + 1] = 't'
+    return None
+
+print(dfs(normal, normalStart, normalEnd))
+print(dfs(long, longStart, longEnd))
