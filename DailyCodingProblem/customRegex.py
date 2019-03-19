09bc5f3014e5bf4e1dd86136c91d26c82e323685
Paul <Neminem1203@users.noreply.github.com>
2019-03-19 00:09:31 -0400

    finished star but still incomplete
    i need to make sure it checks all the possibilities. right now it only finds the first possibility at line 30.

--- DailyCodingProblem/customRegex.py
+++ DailyCodingProblem/customRegex.py
@@ -12,7 +12,29 @@ Given the regular expression ".*at" and the string "chat", your function should 
 '''
 
 def starCheck(expressionList, string):
-    return False
+    check = False
+    while(expressionList):
+        print(string)
+        check = True
+        if(expressionList[0] == "*"):
+            expressionList.pop(0)
+            if(not expressionList):
+                return True
+        elif(expressionList[0] == "."):
+            if(string):
+                expressionList.pop(0)
+                string = string[1:]
+            else:
+                return False
+        else:
+            ind = string.find(expressionList[0])
+            if(ind >= 0):
+                string = string[ind+len(expressionList[0]):]
+                expressionList.pop(0)
+    if(string and check):
+        return False
+    else:
+        return True
 
 
 def customRegex(expression, string):
@@ -31,16 +53,19 @@ def customRegex(expression, string):
 
     while(string and newExpression):
         if(newExpression[0] == '.'):
-            string=string[1:]
-            newExpression.pop(0)
+            if(string):
+                string=string[1:]
+                newExpression.pop(0)
+            else:
+                return False
         elif(newExpression[0] != "*"):
-            if(newExpression[0] != string[:len(newExpression[0])]):
+            if((len(newExpression[0]) > len(string)) or (newExpression[0] != string[:len(newExpression[0])])):
                 return False
             else:
                 string = string[len(newExpression[0]):]
                 newExpression.pop(0)
         else:
-            return starCheck(newExpression, newExpression[1:])
+            return starCheck(newExpression[1:], string)
     if(string):
         return False
     return True
@@ -55,7 +80,7 @@ string2 = "ray"
 string3 = "raymond"
 
 
-# customRegex(regex1, string1)
-# customRegex(regex1, string4)
+print(customRegex(regex1, string1))
+print(customRegex(regex1, string4))
 print(customRegex(regex2, string2))
 print(customRegex(regex2, string3))
