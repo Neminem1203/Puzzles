2cb6d31667b9e38a11c157aaddd4fa82603cf10f
Paul <Neminem1203@users.noreply.github.com>
2019-03-19 22:10:39 -0400

    i found k but forgot i'm trying to remove it

--- DailyCodingProblem/removekLL.py
+++ DailyCodingProblem/removekLL.py
@@ -5,4 +5,46 @@ The list is very long, so making more than one pass is prohibitively expensive.
 
 Do this in constant space and in one pass.
 '''
+import random
+class linkedList:
 
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+    def insert(self, value):
+        if(self.next):
+            self.next.insert(value)
+        else:
+            newNode = linkedList(value)
+            self.next = newNode
+
+
+
+def removeKFromLL(LL, K):
+    point = LL
+    while(K>0):
+        point = point.next
+        K-=1
+    behindPointer = LL
+    while(point.next):
+        point = point.next
+        behindPointer = behindPointer.next
+    return behindPointer.value
+
+
+zelda = linkedList(random.randint(1, 1000))
+
+listLen = 100
+findK = 25
+if(findK == listLen):
+    print("ANSWER: ", end=" ")
+    print(zelda.value)
+for i in range(listLen):
+    randomNum = random.randint(1, 1000)
+    if(i == listLen-findK-1):
+        print("ANSWER: ",end=" ")
+        print(randomNum)
+    zelda.insert(randomNum)
+print()
+print(removeKFromLL(zelda, findK))
