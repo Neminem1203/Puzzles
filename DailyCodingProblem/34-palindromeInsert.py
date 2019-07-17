1d8ea7fdaf8449d1cd81d2865318f1a9f3c62c02
Paul <Neminem1203@users.noreply.github.com>
2019-07-17 00:06:18 -0400

    Create 34-palindromeInsert.py
    finished 90% of the program. I have a breakTest that I need to fix

---
+++ DailyCodingProblem/34-palindromeInsert.py
@@ -0,0 +1,48 @@
+'''
+Given a string, find the palindrome that can be made by inserting the fewest number of characters as possible anywhere in the word. If there is more than one palindrome of minimum length that can be made, return the lexicographically earliest one (the first one alphabetically).
+
+For example, given the string "race", you should return "ecarace", since we can add three letters to it (which is the smallest amount to make a palindrome). There are seven other palindromes that can be made from "race" by adding three letters, but "ecarace" comes first alphabetically.
+
+As another example, given the string "google", you should return "elgoogle".
+'''
+
+def palindromeInsert(word):
+    checkInd = 0
+    newWord = ""
+    dupeWords = []
+    for i in word:
+        newWord += i
+    while(checkInd < len(newWord)):
+        if newWord[checkInd] in newWord[checkInd:]:
+            for ind in range(len(newWord)-1, checkInd, -1):
+                if(newWord[ind] == newWord[checkInd]):
+                    dupeWords += [newWord[checkInd]]
+                    newWord = newWord[:checkInd] + newWord[(checkInd+1):ind] + newWord[(ind+1):]
+                    checkInd -= 1
+                    break
+        checkInd += 1
+    palindrome = ""
+    while(dupeWords != [] and newWord != ""):
+        if(word[-1] == dupeWords[0]):
+            palindrome += dupeWords[0]
+            dupeWords = dupeWords[1:]
+            word = word[:-1]
+            continue
+        palindrome += newWord[-1]
+        newWord = newWord[:-1]
+        word = word[:-1]
+    if(dupeWords == []):
+        palindrome += newWord[-1:0:-1]
+        palindrome = palindrome + newWord[0] + palindrome[::-1]
+    if(newWord == ""):
+        for i in dupeWords:
+            palindrome += i
+        palindrome = palindrome + palindrome[::-1]
+    print(palindrome)
+
+breakTest = "ramgrma" # This should be ramrgrmar but my program gives me grammarg which is completely wrong
+secondTest = "ragamaror"
+palindromeInsert(breakTest)
+palindromeInsert(secondTest)
+palindromeInsert("google")
+palindromeInsert("race")
