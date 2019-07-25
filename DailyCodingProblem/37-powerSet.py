a4a1135776c8d16fc75d91da4198575b2e8b44b0
Paul <Neminem1203@users.noreply.github.com>
2019-07-25 16:49:05 -0400

    finished powerSet
    this took longer than one day. i had to find a way to get the combo correctly

---
+++ DailyCodingProblem/37-powerSet.py
@@ -0,0 +1,20 @@
+
+
+def powerSet(originalSet):
+    newSet = []
+    def combo(stack, frm, to, len):
+        if(len != 1):
+            for num in range(frm, to):
+                combo(stack + [num], num+1, to, len-1)
+        else:
+            newStack = []
+            for stackNum in stack:
+                newStack.append(originalSet[stackNum])
+            for num in range(frm, to):
+                newSet.append(newStack + [originalSet[num]])
+    for i in range(len(originalSet)+1):
+        combo([], 0, len(originalSet), i)
+    # print(newSet)
+    return newSet
+
+print(powerSet([5,7,6,4,1]))
