892eb82b280143182ed32c7b4c60d66c26bfa89b
Paul <Neminem1203@users.noreply.github.com>
2019-08-21 01:08:04 -0400

    finished gameOfLife
    procrastinated this project long enough. i didn't really optimize this because it requires a lot of memory if you have a lot of live cells.

--- DailyCodingProblem/39-gameOfLife.py
+++ DailyCodingProblem/39-gameOfLife.py
@@ -11,3 +11,86 @@ Implement Conway's Game of Life. It should be able to be initialized with a star
 
 You can represent a live cell with an asterisk (*) and a dead cell with a dot (.).
 '''
+
+
+'''
+testLife
+
+* * * .
+. * * *
+* . * *
+. . . .
+
+'''
+def visuaLife(array):
+    minX = 100
+    minY = 100
+    maxX = 0
+    maxY = 0
+    adjustX = 0
+    adjustY = 0
+    for i in array:
+        if i[0] < minX:
+            minX = i[0]
+        if i[0] > maxX:
+            maxX = i[0]
+        if i[1] < minY:
+            minY = i[1]
+        if i[1] > maxY:
+            maxY = i[1]
+
+    #Takes less memory
+    #doesn't create an array for me to use
+    # for i in range(minX, maxX+1):
+    #     for j in range(minY, maxY+1):
+    #         if [i,j] in array:
+    #             print('*', end=" ")
+    #         else:
+    #             print('.', end=" ")
+    #     print()
+
+    #Uses more memory
+    #makes program easier by returning an array
+    if(minX < 0):
+        adjustX -= minX
+        maxX -= minX
+        minX = 0
+
+    if(minY < 0):
+        adjustY -= minY
+        maxY -= minY
+        minY = 0
+    visualArray = [['.' for x in range(minY, maxY+1)] for y in range(minX, maxX+1)]
+    for i in array:
+        visualArray[i[0] + adjustX][i[1]+adjustY] = '*'
+    # for i in visualArray:
+    #     print(i, )
+    return visualArray
+
+testLife = [[0,0],[1,3],[0,1],[2,2],[2,3],[0,2],[1,1],[1,2],[2,0],[5,4]]
+
+
+arrayOfLife = visuaLife(testLife)
+lenX = len(arrayOfLife)
+lenY = len(arrayOfLife[0])
+while(any('*' in row for row in arrayOfLife)):
+    remove1 = True
+    for i in range(lenX):
+        for j in range(lenY):
+            numNeighbors = 0
+            print(arrayOfLife[i][j],end=" ")
+            if(i > 0 and arrayOfLife[i-1][j] == "*"):
+                numNeighbors += 1
+            if(i < lenX-1 and arrayOfLife[i+1][j] == "*"):
+                numNeighbors += 1
+            if (j > 0 and arrayOfLife[i][j-1] == "*"):
+                numNeighbors += 1
+            if (j < lenY - 1 and arrayOfLife[i][j+1] == "*"):
+                numNeighbors += 1
+
+            if(arrayOfLife[i][j] == "*" and numNeighbors < 2 or numNeighbors > 3):
+                arrayOfLife[i][j] = "."
+            if(arrayOfLife[i][j] == "." and numNeighbors == 3):
+                arrayOfLife[i][j] = "*"
+        print()
+    print()
