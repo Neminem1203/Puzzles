c5b9d73d3af25d5f153068518e076b4deafd3442
Paul <Neminem1203@users.noreply.github.com>
2019-08-25 21:30:50 -0400

    Update 40-noTriples.py
    made an O(n) space solution.

--- DailyCodingProblem/40-noTriples.py
+++ DailyCodingProblem/40-noTriples.py
@@ -5,3 +5,21 @@ For example, given [6, 1, 3, 3, 3, 6, 6], return 1. Given [13, 19, 13, 13], retu
 
 Do this in O(N) time and O(1) space.
 '''
+'''
+not sure how to make it O(1) space, but here's my attempt with O(n)
+bitwise operator xor would work with duplicates but not sure how to deal with triples
+'''
+
+def noTriples(array):
+    dupes = {}
+    for i in array:
+        if i in dupes:
+            dupes[i] += 1
+        else:
+            dupes[i] = 1
+    for i in dupes:
+        if dupes[i] == 1:
+            return i
+
+print(noTriples([6,1,3,3,3,6,6]))
+print(noTriples([13,19,13,13]))
