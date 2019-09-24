c0bda74d053e36ac9ad15e1d564f06909ccd2e62
Paul <Neminem1203@users.noreply.github.com>
2019-09-24 19:13:59 -0400

    Finished flightitinerary. deleted linkedlist class
    i deleted linkedlist because as i built the program, i realized i was overthinking it....

--- DailyCodingProblem/41-flightItinerary.py
+++ DailyCodingProblem/41-flightItinerary.py
@@ -8,67 +8,25 @@ Given the list of flights [('SFO', 'COM'), ('COM', 'YYZ')] and starting airport 
 Given the list of flights [('A', 'B'), ('A', 'C'), ('B', 'C'), ('C', 'A')] and starting airport 'A', you should return the list ['A', 'B', 'C', 'A', 'C'] even though ['A', 'C', 'A', 'B', 'C'] is also a valid itinerary. However, the first one is lexicographically smaller.
 '''
 
-class linkedList:
-    val = ""
-    next = None
-    def __init__(self, val):
-        self.val = val
-
-    def print(self):
-        print(self.val,end="")
-        if(self.next != None):
-            print("",end=", ")
-            self.next.print()
-        else:
-            print()
-
-    def insert(self, val):
-        if(self.next == None):
-            if(self.val == None):
-                self.val = val
-                return
-            self.next = linkedList(val)
-        else:
-            self.next.insert(val)
-
-    # alphabetical insert
-    def aInsert(self, val):
-        if(val < self.val):
-            newLink = linkedList(self.val)
-            self.val = val
-            self.next = newLink
-        else:
-            if(self.next == None):
-                self.next = linkedList(val)
-            else:
-                self.next.insert(val)
-
-    # array insert
-    # def insert(self, arrayVal):
-    #     if(arrayVal == []):
-    #         return
-    #     if(self.next == None):
-    #         reference = self
-    #         for i in arrayVal:
-    #             reference.next = linkedList(i)
-    #             reference = reference.next
-    #     else:
-    #         self.next.insert(arrayVal)
-
-    def pop(self):
-        # if(self.next == None):
-        #     retVal = self.val
-        #     self.val = None
-        #     return retVal
-        if(self.next.next == None):
-            retVal = "" + self.next.val
-            del(self.next)
-            return(retVal)
-        else:
-            return(self.next.pop())
-
-
 def flightItinerary(flights, origin):
-    
+    def bestRoute(tripSoFar, remainingFlights):
+        if(remainingFlights == []):
+            return tripSoFar
+        for path_num, flight in enumerate(remainingFlights):
+            if(flight[0] == tripSoFar[-1]):
+                testRoute = bestRoute(tripSoFar+[flight[1]], remainingFlights[:path_num] + remainingFlights[path_num+1:])
+                if(testRoute != []):
+                    return testRoute
+        return []
 
-flightItinerary([('SFO', 'HKO'), ('YYZ', 'SFO'), ('YUL', 'YYZ'), ('HKO', 'ORD')], 'YUL')
+    for path_num, flight in enumerate(flights):
+        if(flight[0] == origin):
+            returnRoute = bestRoute([flight[1]], flights[:path_num] + flights[path_num + 1:])
+            if(returnRoute != []):
+                return [origin]+returnRoute
+    return None
+
+
+print(flightItinerary([('SFO', 'HKO'), ('YYZ', 'SFO'), ('YUL', 'YYZ'), ('HKO', 'ORD')], 'YUL'))
+print(flightItinerary([('SFO', 'COM'), ('COM', 'YYZ')], 'COM'))
+print(flightItinerary([('A', 'B'), ('A', 'C'), ('B', 'C'), ('C', 'A')], 'A'))
