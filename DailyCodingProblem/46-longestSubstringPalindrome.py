6e579ccfaab5864d2c4ed6c77e61268e6ce1cb63
Paul <Neminem1203@users.noreply.github.com>
2019-09-25 19:02:59 -0400

    Update 46-longestSubstringPalindrome.py
    this was made easier because the substring has to be contiguous

--- DailyCodingProblem/46-longestSubstringPalindrome.py
+++ DailyCodingProblem/46-longestSubstringPalindrome.py
@@ -3,3 +3,22 @@ Given a string, find the longest palindromic contiguous substring. If there are 
 
 For example, the longest palindromic substring of "aabcdcb" is "bcdcb". The longest palindromic substring of "bananas" is "anana".
 '''
+
+def longestSubstringPalindrome(string):
+    strings = [string]
+    while(strings != []):
+        # print(strings)
+        left = int(len(strings[0])/2)
+        right = len(strings[0])-left-1
+        # print(strings[0][:left], strings[0][-1:right:-1])
+        if(strings[0][:left] == strings[0][-1:right:-1]):
+            return strings[0]
+        else:
+            if(strings[0][1:] != ''):
+                strings += [strings[0][1:]]
+            if(strings[0][:-1] != ''):
+                strings += [strings[0][:-1]]
+            strings = strings[1:]
+
+print(longestSubstringPalindrome("aabcdcb"))
+print(longestSubstringPalindrome("bananas"))
