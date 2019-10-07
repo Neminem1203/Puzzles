25dd9746705edf81feebe66e457bb3d3c08af3d8
Paul <Neminem1203@users.noreply.github.com>
2019-10-07 19:06:44 -0400

    Create 61-efficientPow.py
    this is pretty efficient based on my count of operations

---
+++ DailyCodingProblem/61-efficientPow.py
@@ -0,0 +1,31 @@
+'''
+Implement integer exponentiation. That is, implement the pow(x, y) function, where x and y are integers and returns x^y.
+
+Do this faster than the naive method of repeated multiplication.
+
+For example, pow(2, 10) should return 1024.
+'''
+import math
+operations = 0 # number of operations of getters and setters excluding the operations variable
+
+def pow(base, exp):
+    global operations
+    returnVal = base # one operation
+    repeat = math.floor(math.log(exp)/math.log(2)) # I don't know the number of operations from these formulas
+    operations = 2
+    iterations = 1
+    for i in range(repeat):
+        operations += 2
+        returnVal *= returnVal
+        iterations *= 2
+    for i in range(exp-iterations):
+        returnVal *= base
+        iterations += 1
+    return returnVal
+
+print(pow(2,10))
+print("Operations: ", operations)
+print(pow(2,16))
+print("Operations: ", operations)
+print(pow(5, 32))
+print("Operations: ", operations)
