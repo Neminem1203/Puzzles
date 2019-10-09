841855571836ec61932d5abc99346b3751a481bd
Paul <Neminem1203@users.noreply.github.com>
2019-10-09 15:00:08 -0400

    visual update for numOfPaths
    i created a visual representation of how manys paths. the visual representation gets worse when we get to higher numbers. (you could also use the matrix to find answers of other n*m grids)

--- DailyCodingProblem/62-numOfPaths.py
+++ DailyCodingProblem/62-numOfPaths.py
@@ -9,9 +9,33 @@ For example, given a 2 by 2 matrix, you should return 2, since there are two way
 
 Given a 5 by 5 matrix, there are 70 ways to get to the bottom-right
 '''
+debug = True
+if debug:
+    visualMatrix = []
+    def printVisualMatrix():
+        for i in range(len(visualMatrix)-1, -1, -1):
+            print(i,end = "\t")
+        print()
+        for i in range(len(visualMatrix)):
+            for j in visualMatrix[i]:
+                print(j,end="\t")
+            print(len(visualMatrix)-i-1)
+
+    def findAnswer(n,m):
+        x = len(visualMatrix)
+        y = len(visualMatrix[0])
+        if(x > n and y > m):
+            return visualMatrix[x-n-1][y-m-1]
+        else:
+            return "Not in range of previous answer"
 
 def numOfPaths(n, m):
+    if debug:
+        global visualMatrix
+        visualMatrix = [[0 for i in range(m)] for j in range(n)]
     def newPath(x,y, n, m):
+        if debug:
+            global visualMatrix
         if(x == n-1 and y == m-1):
             return 1
 
@@ -20,8 +44,15 @@ def numOfPaths(n, m):
             sum += newPath(x+1,y, n,m)
         if(y < m-1):
             sum += newPath(x,y+1, n,m)
+        if debug:
+            visualMatrix[x][y] = sum
         return sum
     return newPath(0,0,n,m)
 
-print(numOfPaths(2,2))
-print(numOfPaths(5,5))
+print("2*2: ",numOfPaths(2,2))
+if debug: printVisualMatrix()
+print("5*5: ",numOfPaths(5,5))
+if debug: printVisualMatrix()
+print("10*10: ", numOfPaths(10,10))
+if debug: printVisualMatrix()
+if debug: print(findAnswer(6, 5))
