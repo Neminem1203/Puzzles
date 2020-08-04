ae1079a02cf58eb3b40d7f5ffb6ccbeb7ed60f62
Paul <Neminem1203@users.noreply.github.com>
2020-08-04 17:39:38 -0400

    Edge Case Added to Highest Product of Three

--- highestProductOfThree.py
+++ highestProductOfThree.py
@@ -28,9 +28,13 @@ def largestOfThreeProducts(intList):
         highest_negative = negatives[-1] * negatives[-2]
     if len(positives) < 3:
         return positives[-1] * highest_negative
+    if positives[0] == 0:
+        return highest_negative * positives[-1]
     if len(positives) > 1:
         highest_positive = positives[-1] * positives[-2]
     return highest_negative * positives[-1] if highest_negative > highest_positive else highest_positive * positives[0]
 
 print(largestOfThreeProducts([10, -11, 7, -10, 5, -6, -12, 2, 12]))
 print(largestOfThreeProducts([12, 11, -5, -2]))
+print(largestOfThreeProducts([0, 5, -5, -2, 0]))
+print(largestOfThreeProducts([5, 5, -5, -2, 0]))
