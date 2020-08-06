d4d05c5dc3f6f7f90e74ebc3fb987a8fa3ac0048
Paul <Neminem1203@users.noreply.github.com>
2020-08-06 17:03:12 -0400

    Minor Updates

--- longestIncreasingSubsequence.py
+++ longestIncreasingSubsequence.py
@@ -6,14 +6,17 @@ For example, given the array [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 
 '''
 
 
-
+count = 0
 if False:
-    # Suboptimal Solution
+    # Suboptimal. Doesn't work for all test cases. Probably a better way to implement in the future
     def longestInreasingSubsequence(array):
+        global count
         longest = 0
         longest_arr = []
         for num in array:
+            count += 1
             while len(longest_arr) > 0 and longest_arr[-1] > num:
+                count += 1
                 longest_arr = longest_arr[:-1]
             longest_arr += [num]
             if len(longest_arr) > longest:
@@ -24,10 +27,12 @@ if False:
 else:
     def longestInreasingSubsequence(array):
         def helper(arr, current=[]):
+            global count
             if len(arr) == 0:
                 return len(current)
             max = 0
             for ind in range(len(arr)):
+                count += 1
                 if len(current) == 0 or arr[ind] > current[-1]:
                     val = helper(arr[ind+1:], current+[arr[ind]])
                     if val > max:
@@ -42,3 +47,4 @@ else:
 
 
 print(longestInreasingSubsequence([0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]))
+print("Count: ",count)
--- multiplicationTableLookup.py
+++ multiplicationTableLookup.py
@@ -26,5 +26,8 @@ def multiplicationTableLookup(n, x):
     for i in range(1,n+1):
         if x % i == 0 and x / i <= n:
             count += 1
+
     return count
 
+
+print(multiplicationTableLookup(25, 65))
