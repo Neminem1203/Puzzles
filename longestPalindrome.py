6a964bcf17a33b13202fcf85479e0f72048a09e6
Paul <Neminem1203@users.noreply.github.com>
2021-01-19 23:16:22 -0500

    second solution for palindrome

--- longestPalindrome.py
+++ longestPalindrome.py
@@ -1,3 +1,4 @@
+# First Solution
 def longestPalindrome(s):
     """
     :type s: str
@@ -27,7 +28,47 @@ def longestPalindrome(s):
                     return_val = substring
     return return_val
 
+# # second solution
+# def longestPalindrome(s):
+#     """
+#     :type s: str
+#     :rtype: str
+#     """
+#     return_val = s[0]
+#     ind = 0
+#     double_middle = False
+#     while ind < len(s):
+#         left = 1
+#         right = 1
+#         if double_middle:
+#             right = 2
+#         while True:
+#             l = ind - left
+#             r = ind + right
+#             if l < 0 or r > len(s):
+#                 break
+#             ss = s[l:r + 1]
+#             print(ss)
+#             if ss == ss[::-1]:
+#                 if len(ss) > len(return_val):
+#                     return_val = ss
+#             else:
+#                 break
+#             left += 1
+#             right += 1
+#         if double_middle:
+#             double_middle = False
+#         elif ind + 1 < len(s) and s[ind] == s[ind + 1]:
+#             double_middle = True
+#             continue
+#         ind += 1
+#
+#     return return_val
+
 print(longestPalindrome("cdbab"))
 print(longestPalindrome("ac"))
+print(longestPalindrome("babad"))
+print(longestPalindrome("aacabdkacaa"))
+print(longestPalindrome("bacabab"))
 print(longestPalindrome("dqmvxouqesajlmksdawfenyaqtnnfhmqbdcniynwhuywucbjzqxhofdzvposbegkvqqrdehxzgikgtibimupumaetjknrjjuygxvncvjlahdbibatmlobctclgbmihiphshfpymgtmpeneldeygmzlpkwzouvwvqkunihmzzzrqodtepgtnljribmqneumbzusgppodmqdvxjhqwqcztcuoqlqenvuuvgxljcnwqfnvilgqrkibuehactsxphxkiwnubszjflvvuhyfwmkgkmlhmvhygncrtcttioxndbszxsyettklotadmudcybhamlcjhjpsmfvvchduxjngoajclmkxiugdtryzinivuuwlkejcgrscldgmwujfygqrximksecmfzathdytauogffxcmfjsczaxnfzvqmylujfevjwuwwaqwtcllrilyncmkjdztleictdebpkzcdilgdmzmvcllnmuwpqxqjmyoageisiaeknbwzxxezfbfejdfausfproowsyyberhiznfmrtzqtgjkyhutieyqgrzlcfvfvxawbcdaawbeqmzjrnbidnzuxfwnfiqspjtrszetubnjbznnjfjxfwtzhzejahravwmkakqsmuynklmeffangwicghckrcjwtusfpdyxxqqmfcxeurnsrmqyameuvouqspahkvouhsjqvimznbkvmtqqzpqzyqivsmznnyoauezmrgvproomvqiuzjolejptuwbdzwalfcmweqqmvdhejguwlmvkaydjrjkijtrkdezbipxoccicygmmibflxdeoxvudzeobyyrutbcydusjhmlwnfncahxgswxiupgxgvktwkdxumqp"))
 
