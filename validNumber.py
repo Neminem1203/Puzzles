3e9b993ed7e464b2cf194460f2104f971cfc83d4
Paul <Neminem1203@users.noreply.github.com>
2021-02-16 22:01:46 -0500

    hard leetcode valid number solved

--- validNumber.py
+++ validNumber.py
@@ -5,18 +5,19 @@ def isNumber(s):
     :rtype: bool
     """
 
-    def isInt(char):
+    def isInt(char): # checks if char passed in is a number
         return ord('0') <= ord(char) <= ord('9')
 
     # 0 = full number, 1 = decimal, 2 = exponent
     has_num = False
     state = 0
     ind = 0
-    if s[0] in ['-', '+']:
+    if s[0] in ['-', '+']: # checks for a sign number
         ind += 1
+
     while ind < len(s):
         char = s[ind]
-        if state == 0:
+        if state == 0: # full number
             if char == ".":
                 state = 1
             elif has_num and char.lower() == "e":
@@ -28,7 +29,7 @@ def isNumber(s):
                 return False
             else:
                 has_num = True
-        elif state == 1:
+        elif state == 1: # decimal
             if has_num and char.lower() == "e":
                 state = 2
                 has_num = False
@@ -38,7 +39,7 @@ def isNumber(s):
                 return False
             else:
                 has_num = True
-        elif state == 2:
+        elif state == 2: #exponent
             if not isInt(char):
                 return False
             else:
