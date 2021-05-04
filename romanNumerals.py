b12b5b2593cea2f74cd15ca5c77c28f7a2b2137d
Paul <Neminem1203@users.noreply.github.com>
2021-05-04 18:27:00 -0400

    roman numerals in python

---
+++ romanNumerals.py
@@ -0,0 +1,21 @@
+roman_val = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M":1000}
+
+def romanToDec(roman):
+    sum = 0
+    ind = 0
+    while ind < len(roman)-1:
+        char = roman[ind]
+        val = roman_val[char]
+        next_val = roman_val[roman[ind+1]]
+        if next_val <= val:
+            sum += val
+        else:
+            sum -= val
+        ind += 1
+    return sum + roman_val[roman[-1]]
+
+         # 4      49       83        94      2021     86
+romans = ["IV", "XLIX", "LXXXIII", "XCIV", "MMXXI", "LXXXIV"]
+
+for roman in romans:
+    print(romanToDec(roman))
