03371b085a39b491ca13c882905fb9bb6f8a06cf
Paul <Neminem1203@users.noreply.github.com>
2021-10-26 23:05:35 -0400

    DRYed up Change Maker

--- change_maker.py
+++ change_maker.py
@@ -1,3 +1,16 @@
+def iteration_generator(coin_list, main_list):
+    new_list = []
+    for coin in coin_list:
+        for values in main_list:
+            new_list.append(values+coin)
+    return new_list
+
+def counter(main_list):
+    ret_list = [0 for _ in range(main_list[-1]+1)]
+    for i in main_list:
+        ret_list[i] += 1
+    return ret_list
+
 def target_finder(coins, target):
     '''
     Makes target value with the coins given
@@ -12,21 +25,16 @@ def target_finder(coins, target):
         if(coin_value <= 0):
             raise "All coin values must be > 0"
         coin_list = [0] # you can always make $0
+
         # add up to the target value
         curr_val = coin_value
         while curr_val <= target:
             coin_list.append(curr_val)
             curr_val += coin_value
-        # the new list we will replace main with
-        new_list = []
-        for coin in coin_list:
-            for values in main_list:
-                new_list.append(values+coin)
-        main_list = new_list
+
+        main_list = iteration_generator(coin_list, main_list)
     # how many diff iterations you can make that amount of money in cents
-    coin_count_list = [0 for _ in range(main_list[-1]+1)]
-    for i in main_list:
-        coin_count_list[i] += 1
+    coin_count_list = counter(main_list)
     return(coin_count_list[target]) # how to make change for target
 
 def change_maker(coins, target):
@@ -41,19 +49,15 @@ def change_maker(coins, target):
         if(coin_value <= 0):
             raise "All coin values must be > 0"
         coin_list = [0] # you can always make $0
+
         # add up to the target value
         for i in range(1, coin_count+1):
             coin_list.append(coin_value * i)
-        new_list = []
-        for coin in coin_list:
-            for values in main_list:
-                new_list.append(values+coin)
-        main_list = new_list
+
+        main_list = iteration_generator(coin_list, main_list)
     # how many diff iterations you can make that amount of money in cents
-    coin_count_list = [0 for _ in range(main_list[-1]+1)]
-    for i in main_list:
-        coin_count_list[i] += 1
+    coin_count_list = counter(main_list)
     return(coin_count_list[target]) # how to make change for target
 
-print(change_maker([[1,3],[3,2]], 6)) # 1 1 1 3 3
+print(change_maker([[1,3],[3,2]], 6)) # 1 1 1 3 3 making 6
 print(target_finder([1,5,10,25,50,100], 100)) # all coins making change for $1
